d65717136bd7263997ef4066044a6949bc37870c
Kelsey Huelsman <111786534+HuelsmanKelsey@users.noreply.github.com>
2026-09-03 17:39:01 -0400

    created GetVeg code w plain language summary from readme

--- GetSpectra.R
+++ GetSpectra.R
@@ -3,3 +3,4 @@
 # Choose a site/location and request from API to download
 # open and extract necessary info, interpret the data
 # Visualize reflectances and save a simple (5-band) preliminary map
+
---
+++ GetVeg.R
@@ -0,0 +1,24 @@
+#Get Veg lets you
+# Choose a site/location and filter the vegetation dataset
+# Crosswalk / interpret the data
+# Visualize and understand the distributions 
+
+# Summarize: 
+# Whole plot, subplot size-specific analyses of % cover
+# relative frequency for plot and larger subplots
+
+# capturing vegetation “vibe” using PA or % cover:
+
+# among (matrix rows) in terms of cover and composition, 
+#which is then used to characterize each organizational level: 
+ # [a diagram of subplot, plot, site, domain, all NEON sites]
+#and what we're dealing w here...
+
+#a single site for now
+
+#output:
+#loadings of each species in each PC; score of each component in each subplot /. plot
+
+#show clustering by species groups hopefully
+
+#consider how this might translate to a map, spatial / grid patterns added to what we already see
