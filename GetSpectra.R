4b7f2e72a9c42df09fc84be77355955fb028e0bf
Kelsey Huelsman <111786534+HuelsmanKelsey@users.noreply.github.com>
2026-09-03 17:33:13 -0400

    created GetSpectra code with plain language description from readme

---
+++ GetSpectra.R
@@ -0,0 +1,5 @@
+# Get Spectra lets you
+
+# Choose a site/location and request from API to download
+# open and extract necessary info, interpret the data
+# Visualize reflectances and save a simple (5-band) preliminary map
