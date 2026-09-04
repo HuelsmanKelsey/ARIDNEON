3c8c3550cdf3eab0e13120b7ca03d794eb7a98c6
Kelsey Huelsman <111786534+HuelsmanKelsey@users.noreply.github.com>
2026-09-04 10:10:40 -0400

    added file for using veg field data provided by David Augustine

---
+++ DAVeg.R
@@ -0,0 +1,7 @@
+
+# David Augustine's data:
+#cover for 48 polygons from a few days in June 2026:
+plot_surveys <- read.csv('Augustine_Plot_Surveys.csv')
+
+#pure shrub and bare soil points and polygons
+#pure_ems <- read.csv('Augustine_Pure_EMs.csv')
