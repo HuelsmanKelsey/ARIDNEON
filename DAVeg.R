3190170149f054476e1221274bd38cff944aca82
Kelsey Huelsman <111786534+HuelsmanKelsey@users.noreply.github.com>
2026-09-08 21:52:46 -0400

    DAVeg is for David Augustine group data from CPER

--- DAVeg.R
+++ DAVeg.R
@@ -5,3 +5,12 @@ plot_surveys <- read.csv('Augustine_Plot_Surveys.csv')
 
 #pure shrub and bare soil points and polygons
 #pure_ems <- read.csv('Augustine_Pure_EMs.csv')
+
+
+
+#shape files for David Augustine's data (although I am unclear how they relate)
+LL_pt <- sf::st_zm(sf::st_read("/Users/khuelsma/ARIDNEON/Augustine_VegPoint_2026.shp/VegPOINT_2026.shp"))
+pt_vect <- terra::vect(LL_pt)
+
+LL_poly <- sf::st_zm(sf::st_read("/Users/khuelsma/ARIDNEON/Augustine_VegPolygon_2026/VegPolygon2026.shp"))
+poly_vect <- terra::vect(LL_poly)
